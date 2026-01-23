d4cb8108c235dff0a8a524b7226b7f02993e352c
bitcoinaudio <bitcoinaudio@gmail.com>
2026-01-23 18:11:36 -0500

    Update InspiraRoutes.tsx

--- src/InspiraRoutes.tsx
+++ src/InspiraRoutes.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route, Navigate } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 
 import InspiraLanding from './pages/InspiraLanding';
 import AIGenerator from './pages/AIGenerator';
